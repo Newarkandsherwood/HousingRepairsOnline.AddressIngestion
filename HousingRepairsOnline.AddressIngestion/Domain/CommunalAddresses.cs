c122aa4b10af0348919ce9bf6e20c9771309e161
maysakanoni <maysa@madetech.com>
2022-08-09 16:31:54 +0100

    write to cosmos db and make variables configurable
    Co-authored-by: Mark Warriner <mark.warriner@madetech.com>

--- HousingRepairsOnline.AddressIngestion/Domain/CommunalAddresses.cs
+++ HousingRepairsOnline.AddressIngestion/Domain/CommunalAddresses.cs
@@ -1,10 +1,14 @@
+using CsvHelper.Configuration.Attributes;
+
 namespace HousingRepairsOnline.AddressIngestion.Domain
 {
     public class CommunalAddresses
     {
-        public string Id { get; set;}
+        [Index(0)]
         public int PlaceReference { get; set; }
+        [Index(1)]
         public string AddressLine { get; set; }
+        [Index(2)]
         public string PostCode { get; set; }
     }
 }
--- HousingRepairsOnline.AddressIngestion/Function.cs
+++ HousingRepairsOnline.AddressIngestion/Function.cs
@@ -6,31 +6,32 @@ using System.Threading.Tasks;
 using CsvHelper;
 using HousingRepairsOnline.AddressIngestion.Domain;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace HousingRepairsOnline.AddressIngestion
 {
     public static class Function
     {
-        [FunctionName("Function")]
+        [FunctionName("IngestAddresses")]
         public static async Task RunAsync(
             [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
-            [Blob("insight-report-addresses/communal.csv", FileAccess.Read,  Connection = "AzureWebJobsStorage")] Stream inputStream,
-            // [CosmosDB(
-            //     databaseName: "TestAddresses",
-            //     collectionName: "Communal",
-            //     ConnectionStringSetting = "CosmosDBConnection")]
-            // IAsyncCollector<CommunalAddresses> communalAddressesOut,
+            [Blob("%CommunalBlobPath%", FileAccess.Read,  Connection = "AzureWebJobsStorage")] Stream inputStream,
+            [CosmosDB(
+                databaseName : "%DatabaseName%",
+                collectionName: "%CollectionName%",
+                ConnectionStringSetting = "CosmosDBConnection")]
+            IAsyncCollector<CommunalAddresses> communalAddressesOut,
             ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
-            log.LogInformation(inputStream.Length.ToString());
 
             using var reader = new StreamReader(inputStream);
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
             var records = csv.GetRecords<CommunalAddresses>();
-            log.LogInformation(records.First().ToString());
+            foreach (var record in records)
+            {
+                await communalAddressesOut.AddAsync(record);
+            }
         }
     }
 }
