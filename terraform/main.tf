0725d9b2182cea6bf97d81478581991b48526b27
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-09-06 15:03:29 +0100

    Add role assignment for contributor role
    Co-authored-by: maysakanoni <maysa@madetech.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,4 +34,23 @@ resource "azurerm_windows_function_app" "example" {
   }
 }
 
+data "azurerm_subscription" "primary" {
+}
 
+data "azurerm_storage_account" "example" {
+  name                = var.storage-account
+  resource_group_name = var.resource-group
+}
+
+resource "azurerm_role_assignment" "ingest_addresses_contributor" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_windows_function_app.example.identity[0].principal_id
+}
+
+# Allow our function's managed identity to have r/w access to the storage account
+resource "azurerm_role_assignment" "ingest_addresses" {
+  principal_id         = azurerm_windows_function_app.example.identity[0].principal_id
+  scope                = data.azurerm_storage_account.example.id
+  role_definition_name = "Storage Blob Data Reader"
+}
