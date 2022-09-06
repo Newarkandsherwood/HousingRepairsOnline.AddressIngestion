a0e95c3bdf5f1fcfba2ef6092a31da8809cd6f19
maysakanoni <maysa@madetech.com>
2022-09-06 14:32:41 +0100

    format
    Co-authored-by: Renny Fadoju <32823756+renny4real@users.noreply.github.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,39 @@
 variable "project" {
-    type = string
-    description = "Housing Repairs Online"
+  type        = string
+  description = "Housing Repairs Online"
 }
 
 variable "environment" {
-    type = string
-    description = "Environment (dev / stage / prod)"
+  type        = string
+  description = "Environment (dev / stage / prod)"
 }
 
 variable "location" {
-    type = string
-    description = "Azure region to deploy module to"
+  type        = string
+  description = "Azure region to deploy module to"
 }
 
 variable "resource-group" {
-    type = string
-    description = "Resource group name"
+  type        = string
+  description = "Resource group name"
 }
 
 variable "storage-account" {
-    type = string
-    description = "Storage account name"
+  type        = string
+  description = "Storage account name"
 }
 
 variable "app-service-plan" {
-    type = string
-    description = "App service plan name"
+  type        = string
+  description = "App service plan name"
 }
 
 variable "storage-account-primary-access-key" {
-    type = string
-    description = "storage account primary access key"
+  type        = string
+  description = "storage account primary access key"
 }
 
 variable "storage-container-name" {
-    type = string
-    description = "storage container name"
+  type        = string
+  description = "storage container name"
 }
