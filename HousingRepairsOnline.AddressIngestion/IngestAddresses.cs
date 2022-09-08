6cd13af9e5e368bc676d691e5d14e44a0f086f5b
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-09-08 15:27:47 +0100

    Test function without using helper method abstraction

--- HousingRepairsOnline.AddressIngestion/IngestAddresses.cs
+++ HousingRepairsOnline.AddressIngestion/IngestAddresses.cs
@@ -2,6 +2,7 @@ namespace HousingRepairsOnline.AddressIngestion
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Threading.Tasks;
     using HACT.Dtos;
     using HousingRepairsOnline.AddressIngestion.Helpers;
@@ -47,8 +48,38 @@ namespace HousingRepairsOnline.AddressIngestion
 
             var addresses = Mapper.CsvInputStreamToAddresses(inputStream);
             var propertyAddresses = Mapper.ToHactPropertyAddresses(addresses, housingProvider);
-            var insertAddressesToCosmosDB = new InsertAddressesToCosmosDb(propertyAddressesOut, log);
-            await insertAddressesToCosmosDB.Execute(propertyAddresses);
+
+            // var attributes = new Attribute[]
+            // {
+            //     new CosmosDBAttribute(databaseName, collectionName)
+            // };
+            // var propertyAddressesOut = await binder.BindAsync<IAsyncCollector<PropertyAddress>>(attributes.First());
+            // var insertAddressesToCosmosDB = new InsertAddressesToCosmosDb(propertyAddressesOut, log);
+            // await insertAddressesToCosmosDB.Execute(propertyAddresses);
+
+            foreach (var propertyAddress in propertyAddresses)
+            {
+                var isValidData = true;
+                if (propertyAddress.AddressLine.FirstOrDefault() is "" or null)
+                {
+                    log.LogInformation($"AddressLine for property {propertyAddress.Reference.ID} is null or empty");
+                    isValidData = false;
+                }
+                if (string.IsNullOrEmpty(propertyAddress.PostalCode))
+                {
+                    log.LogInformation($"Postalcode for property {propertyAddress.PostalCode} is null or empty");
+                    isValidData = false;
+                }
+                if (string.IsNullOrEmpty(propertyAddress.Reference.ID))
+                {
+                    log.LogInformation($"Property Id is null or empty");
+                    isValidData = false;
+                }
+                if (isValidData)
+                {
+                    await propertyAddressesOut.AddAsync(propertyAddress);
+                }
+            }
         }
     }
 }
